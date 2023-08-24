81347d4ff717c2570029f60c8e5ce2db34ecaed9
luisfp2000 <luisfp2000@gmail.com>
2023-08-24 14:04:15 -0600

    changes

--- demo_fast_api_logging/utilities/CustomLogging.py
+++ demo_fast_api_logging/utilities/CustomLogging.py
@@ -1,9 +1,23 @@
 import logging
 
 class CustomLogging:
-    def __init__(self, logger_name, log_file):
+    def __init__(self, logger_name, log_file, log_level):
+
         self.logger = logging.getLogger(logger_name)
-        self.logger.setLevel(logging.DEBUG)
+
+        if log_level =="INFO":
+            self.logger.setLevel(logging.INFO)
+        elif log_level =="DEBUG":
+            self.logger.setLevel(logging.DEBUG)
+        elif log_level =="ERROR":
+            self.logger.setLevel(logging.ERROR)
+        elif log_level =="WARNING":
+            self.logger.setLevel(logging.WARNING)
+        elif log_level =="CRITICAL":
+            self.logger.setLevel(logging.CRITICAL)
+        else:
+            self.logger.setLevel(logging.INFO)
+
         
         formatter = logging.Formatter('%(asctime)s:%(name)s:%(module)s:%(levelname)s:%(message)s')
         
@@ -19,3 +33,18 @@ class CustomLogging:
     
     def get_logger(self):
         return self.logger
+    
+    def debug(self, msg):
+        self.logger.debug(msg)
+
+    def info(self, msg):
+        self.logger.info(msg)
+
+    def warning(self, msg):
+        self.logger.warning(msg)
+
+    def error(self, msg):
+        self.logger.error(msg)
+
+    def critical(self, msg):
+        self.logger.critical(msg)
